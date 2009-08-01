5718a09ebf7d08ebfc58acf660f5078ae64eef9d
Vlad Ionescu <vlad@lshift.net>
2009-08-01 16:44:12 +0100

    removing all nant stuff; moving all in lib to lib/wix; restructuring apigen and client library in VS projects with customized builds; more restructuring to follow

---
+++ projects/ApiGen/properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ApiGen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LShift Ltd.")]
+[assembly: AssemblyProduct("ApiGen")]
+[assembly: AssemblyCopyright("Copyright ©  2007-2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("047d6662-98f3-412d-b8a1-3a50b45493f4")]
+
