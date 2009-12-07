40c5c0366f3ca6bf7dc196297a5a16d5c88726a7
Matthias Radestock <matthias@lshift.net>
2009-12-07 17:58:17 +0000

    improve display of BrokerUnreachableException - include stack trace of inner exceptions in ToString - expose inner exceptions in standard 'Data' property

--- projects/client/RabbitMQ.Client/src/client/exceptions/BrokerUnreachableException.cs
+++ projects/client/RabbitMQ.Client/src/client/exceptions/BrokerUnreachableException.cs
@@ -85,6 +85,9 @@ namespace RabbitMQ.Client.Exceptions {
         ///each endpoint.</summary>
         public IDictionary ConnectionErrors { get { return m_connectionErrors; } }
 
+        ///<summary>same as ConnectionErrors property</summary>
+        public override IDictionary Data { get { return m_connectionErrors; } }
+
         ///<summary>Construct a BrokerUnreachableException. Expects
         ///maps as per the description of the ConnectionAttempts and
         ///ConnectionErrors properties.</summary>
@@ -103,19 +106,12 @@ namespace RabbitMQ.Client.Exceptions {
             StringBuilder sb = new StringBuilder(base.Message);
             sb.Append("\nEndpoints attempted:\n");
             foreach (DictionaryEntry entry in m_connectionAttempts) {
-                sb.Append("  endpoint=");
-                sb.Append(entry.Key);
-                sb.Append(", attempts=");
-                sb.Append(entry.Value);
-                sb.Append(", outcome=");
-                Exception e = m_connectionErrors[entry.Key] as Exception;
-                if (e == null) {
-                    sb.Append("(null)");
-                } else {
-                    sb.Append(e.Message);
-                }
-                sb.Append("\n");
+                sb.Append("------------------------------------------------\n");
+                sb.Append("endpoint=").Append(entry.Key);
+                sb.Append(", attempts=").Append(entry.Value).Append("\n");
+                sb.Append(m_connectionErrors[entry.Key] as Exception);
             }
+            sb.Append("\n================================================\n");
             sb.Append("Stack trace:\n");
             sb.Append(base.StackTrace);
             return sb.ToString();
