336112d3e1a11e9d9b6eb5e1a50f8ea6d7af05ec
David R. MacIver <david.maciver@lshift.net>
2010-01-26 11:27:11 +0000

    fix API to provide parameterless CreateConnection

--- projects/client/RabbitMQ.Client/src/client/api/AmqpTcpEndpoint.cs
+++ projects/client/RabbitMQ.Client/src/client/api/AmqpTcpEndpoint.cs
@@ -73,6 +73,9 @@ namespace RabbitMQ.Client
     ///</para>
     public class AmqpTcpEndpoint
     {
+        ///<summary>Indicates that the default port for the protocol should be used</summary>
+        public const int UseDefaultPort = -1;
+
         private IProtocol m_protocol;
         ///<summary>Retrieve or set the IProtocol of this AmqpTcpEndpoint.</summary>
         public IProtocol Protocol
@@ -95,7 +98,7 @@ namespace RabbitMQ.Client
         ///port number for the IProtocol to be used.</summary>
         public int Port
         {
-            get { return (m_port == -1) ? m_protocol.DefaultPort : m_port; }
+            get { return (m_port == UseDefaultPort) ? m_protocol.DefaultPort : m_port; }
             set { m_port = value; }
         }
 
