a34b75a5b921b9b76cd08ff21e9376cc96fec047
Alexandru Scvortov <alexandru@rabbitmq.com>
2010-07-08 16:45:44 +0100

    added missing file for Subscriber example

---
+++ projects/examples/client/Subscriber/properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Subscriber")]
+[assembly: AssemblyDescription("SubscriberExample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LShift Ltd.")]
+[assembly: AssemblyProduct("RabbitMQ.Client")]
+[assembly: AssemblyCopyright("Copyright © 2007-2010 LShift Ltd., Cohesive Financial Technologies LLC., and Rabbit Technologies Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12187560-4AE6-40af-A04E-43553FCA6C91")]
