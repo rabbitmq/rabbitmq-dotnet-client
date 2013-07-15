a0f18a76c15d07f8f896ba82bdcdf6a27ee67bc5
Michael Klishin <michael@rabbitmq.com>
2013-07-15 16:39:31 +0400

    Test for connection.[un]blocked: initial version

---
+++ projects/client/Unit/src/unit/TestConnectionBlocked.cs
@@ -0,0 +1,147 @@
+// This source code is dual-licensed under the Apache License, version
+// 2.0, and the Mozilla Public License, version 1.1.
+//
+// The APL v2.0:
+//
+//---------------------------------------------------------------------------
+//   Copyright (C) 2007-2013 VMware, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//---------------------------------------------------------------------------
+//
+// The MPL v1.1:
+//
+//---------------------------------------------------------------------------
+//  The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License
+//  at http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+//  the License for the specific language governing rights and
+//  limitations under the License.
+//
+//  The Original Code is RabbitMQ.
+//
+//  The Initial Developer of the Original Code is VMware, Inc.
+//  Copyright (c) 2007-2013 VMware, Inc.  All rights reserved.
+//---------------------------------------------------------------------------
+
+using NUnit.Framework;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Threading;
+using System.Diagnostics;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Impl;
+using RabbitMQ.Util;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Client.Unit {
+    [TestFixture]
+    public class TestConnectionBlocked {
+
+        Object lockObject = new Object();
+        UTF8Encoding enc = new UTF8Encoding();
+        bool notified = false;
+
+        [Test]
+        public void TestConnectionBlockedNotification()
+        {
+	    ConnectionFactory cf = new ConnectionFactory();
+	    IConnection conn = cf.CreateConnection();
+
+	    conn.ConnectionBlocked   += new ConnectionBlockedEventHandler(HandleBlocked);
+	    conn.ConnectionUnblocked += new ConnectionUnblockedEventHandler(HandleUnblocked);
+
+	    Block();
+	    Publish(conn);
+	    Monitor.Wait(TimeSpan.FromSeconds(10));
+
+	    Assert.IsTrue(notified);
+        }
+
+        public void HandleBlocked(IConnection sender, ConnectionBlockedEventArgs args)
+        {
+	    Unblock();
+        }
+
+
+        public void HandleUnblocked(IConnection sender)
+        {
+	    notified = true;
+	    Monitor.PulseAll(lockObject);
+        }
+
+
+        protected void Block()
+        {
+	    ExecRabbitMQCtl("set_vm_memory_high_watermark 0.000000001");
+        }
+
+        protected void Unblock()
+        {
+	    ExecRabbitMQCtl("set_vm_memory_high_watermark 0.4");
+        }
+
+        protected void ExecRabbitMQCtl(string command)
+        {
+	    if(IsRunningOnMono()) {
+	        ExecCommand("../rabbitmq-server/scripts/rabbitmqctl " + command);
+	    } else {
+		ExecCommand("..\\rabbitmq-server\\scripts\\rabbitmqctl " + command);
+	    }
+	}
+
+        protected void ExecCommand(string command)
+        {
+	    Process proc = new Process();
+	    proc.StartInfo.CreateNoWindow = true;
+
+	    string sh;
+	    string args;
+	    if(IsRunningOnMono()) {
+	        sh   = "/bin/sh";
+		args = "-c " + command;
+	    } else {
+	        sh   = "C:\\winnt\\system32\\cmd.exe";
+		args = "/y /c " + command;
+	    }
+
+	    try {
+	      proc.StartInfo.FileName = sh;
+	      proc.StartInfo.Arguments = args;
+
+	      proc.Start();
+	    } catch (Exception e) {
+	        Console.WriteLine("Failed to run subprocess with args " + args + " : " + e.Message);
+	    }
+	}
+
+        public static bool IsRunningOnMono()
+        {
+	    return Type.GetType("Mono.Runtime") != null;
+	}
+
+        protected void Publish(IConnection conn)
+        {
+	    IModel ch = conn.CreateModel();
+	    ch.BasicPublish("", "amq.fanout", null, enc.GetBytes("message"));
+        }
+    }
+}
