76f6953aefb07535674f5ec1975764b394d42bae
Michael Klishin <michael@rabbitmq.com>
2013-08-02 20:35:44 +0400

    Add some tests for IModel#BasicGet
    They do not reproduce OP's issue in the reported bug but are useful
    as a reference.

---
+++ projects/client/Unit/src/unit/TestBasicGet.cs
@@ -0,0 +1,101 @@
+// This source code is dual-licensed under the Apache License, version
+// 2.0, and the Mozilla Public License, version 1.1.
+//
+// The APL v2.0:
+//
+//---------------------------------------------------------------------------
+//   Copyright (C) 2007-2013 GoPivotal, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//---------------------------------------------------------------------------
+//
+// The MPL v1.1:
+//
+//---------------------------------------------------------------------------
+//  The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License
+//  at http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+//  the License for the specific language governing rights and
+//  limitations under the License.
+//
+//  The Original Code is RabbitMQ.
+//
+//  The Initial Developer of the Original Code is GoPivotal, Inc.
+//  Copyright (c) 2007-2013 GoPivotal, Inc.  All rights reserved.
+//---------------------------------------------------------------------------
+
+using NUnit.Framework;
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Threading;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Impl;
+using RabbitMQ.Util;
+using RabbitMQ.Client.Exceptions;
+
+namespace RabbitMQ.Client.Unit {
+    [TestFixture]
+    public class TestBasicGet : IntegrationFixture {
+
+        [Test]
+        public void TestBasicGetWithEnqueuedMessages() {
+            string queue = "basic.get";
+            UTF8Encoding enc = new UTF8Encoding();
+            Assert.IsTrue(Model.IsOpen);
+
+            Model.QueueDeclare(queue, false, true, false, null);
+            Model.QueuePurge(queue);
+
+            Model.BasicPublish("", queue, null, enc.GetBytes("message"));
+            Thread.Sleep(250);
+
+            Assert.IsNotNull(Model.BasicGet(queue, false));
+            Model.QueuePurge(queue);
+            Assert.IsNull(Model.BasicGet(queue, false));
+            Model.QueueDelete(queue);
+        }
+
+
+        [Test]
+        public void TestBasicGetWithEmptyQueue() {
+            string queue;
+            Assert.IsTrue(Model.IsOpen);
+
+            queue = Model.QueueDeclare();
+
+            Assert.IsNull(Model.BasicGet(queue, false));
+            Model.QueueDelete(queue);
+        }
+
+        [Test]
+        [ExpectedException("RabbitMQ.Client.Exceptions.OperationInterruptedException")]
+        public void TestBasicGetWithClosedChannel() {
+            string queue;
+            Assert.IsTrue(Model.IsOpen);
+
+            queue = Model.QueueDeclare();
+            Model.Close();
+            Assert.IsFalse(Model.IsOpen);
+
+            Model.BasicGet(queue, false);
+        }
+    }
+}
