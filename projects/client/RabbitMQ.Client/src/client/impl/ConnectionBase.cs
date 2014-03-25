90e632dbbf6219b8dcf4e428c151ea21653408e1
Michael Klishin <michael@rabbitmq.com>
2014-03-25 18:03:13 +0400

    Ignore SocketExceptions when shutting down

--- projects/client/RabbitMQ.Client/src/client/impl/ConnectionBase.cs
+++ projects/client/RabbitMQ.Client/src/client/impl/ConnectionBase.cs
@@ -665,7 +665,15 @@ namespace RabbitMQ.Client.Impl
                 // connection closes.
                 if (shutdownCleanly)
                 {
-                    ClosingLoop();
+                    try
+                    {
+                        ClosingLoop();
+                    } catch (SocketException se)
+                    {
+                        // means that socket was closed when frame handler
+                        // attempted to use it. Since we are shutting down,
+                        // ignore it.
+                    }
                 }
 
                 FinishClose();
