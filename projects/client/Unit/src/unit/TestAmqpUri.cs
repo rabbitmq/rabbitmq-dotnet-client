80996d1381188068219ac16dd8f6c40e097d17d7
Matthias Radestock <matthias@rabbitmq.com>
2014-04-10 11:14:54 +0100

    update copyright notice year

--- projects/client/Unit/src/unit/TestAmqpUri.cs
+++ projects/client/Unit/src/unit/TestAmqpUri.cs
@@ -4,7 +4,7 @@
 // The APL v2.0:
 //
 //---------------------------------------------------------------------------
-//   Copyright (C) 2011-2013 GoPivotal, Inc.
+//   Copyright (C) 2011-2014 GoPivotal, Inc.
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
