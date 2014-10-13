312263d291172d6bac9335589dc111921dc1fe72
Michael Klishin <michael@rabbitmq.com>
2014-10-14 01:24:01 +0400

    Doco

--- projects/client/RabbitMQ.Client/src/client/events/RecoveryExceptionEventArgs.cs
+++ projects/client/RabbitMQ.Client/src/client/events/RecoveryExceptionEventArgs.cs
@@ -42,6 +42,10 @@ using System;
 
 namespace RabbitMQ.Client.Events
 {
+    ///<summary>
+    ///Describes an exception that was thrown during
+    ///automatic connection recovery performed by the library.
+    ///</summary>
     public class RecoveryExceptionEventArgs: BaseExceptionEventArgs
     {
         public RecoveryExceptionEventArgs(Exception e) : base(e) {}
