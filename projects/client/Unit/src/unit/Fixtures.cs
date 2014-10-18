382cad4d5a31b2f373be7ec3d92ae50e3518f319
Michael Klishin <michael@rabbitmq.com>
2014-10-18 18:17:39 +0400

    Add assertions since parsing shellout output is quite fragile

--- projects/client/Unit/src/unit/Fixtures.cs
+++ projects/client/Unit/src/unit/Fixtures.cs
@@ -439,6 +439,8 @@ namespace RabbitMQ.Client.Unit
             // line: <rabbit@mercurio.1.11491.0>	58713
             return lines.Select(s => {
               var columns = s.Split('\t');
+              Debug.Assert(!string.IsNullOrEmpty(columns[0]), "columns[0] is null or empty!");
+	      Debug.Assert(!string.IsNullOrEmpty(columns[1]), "columns[1] is null or empty!");
               return new ConnectionInfo(columns[0], Convert.ToUInt32(columns[1]));
             }).ToList();
         }
