f626f7bc3772a881fddeecf633edaf5a8b965b25
Michael Klishin <mklishin@pivotal.io>
2015-04-14 13:30:29 +0300

    Add a test for #58 (it passes)

---
+++ projects/client/Unit/src/unit/TestConnectionChurnHandleLeak.cs
@@ -0,0 +1,71 @@
+﻿// This source code is dual-licensed under the Apache License, version
+// 2.0, and the Mozilla Public License, version 1.1.
+//
+// The APL v2.0:
+//
+//---------------------------------------------------------------------------
+//   Copyright (C) 2007-2014 GoPivotal, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//---------------------------------------------------------------------------
+//
+// The MPL v1.1:
+//
+//---------------------------------------------------------------------------
+//  The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License
+//  at http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+//  the License for the specific language governing rights and
+//  limitations under the License.
+//
+//  The Original Code is RabbitMQ.
+//
+//  The Initial Developer of the Original Code is GoPivotal, Inc.
+//  Copyright (c) 2007-2014 GoPivotal, Inc.  All rights reserved.
+//---------------------------------------------------------------------------
+
+using NUnit.Framework;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace RabbitMQ.Client.Unit
+{
+    [TestFixture]
+    public class TestConnectionChurnHandleLeak : IntegrationFixture
+    {
+        [Test]
+        [Category("RequireSMP")]
+        public void TestHandleLeak()
+        {
+            var cf = new ConnectionFactory();
+            var me = Process.GetCurrentProcess();
+            var n = me.HandleCount;
+            Console.WriteLine("{0} handles before the test...", me.HandleCount);
+            for (var i = 0; i < 1000; i++)
+            {
+                using (var conn = cf.CreateConnection())
+                {
+                }
+            }
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            Console.WriteLine("{0} handles after the test...", me.HandleCount);
+            Assert.That(me.HandleCount, Is.LessThanOrEqualTo(n));
+        }
+    }
+}
