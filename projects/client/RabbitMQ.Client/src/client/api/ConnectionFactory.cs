cd31f99ab1e8cdd55e8a08ce99ba6387bc51b1f5
Michael Klishin <mklishin@pivotal.io>
2015-05-29 15:32:45 +0300

    More docs

--- projects/client/RabbitMQ.Client/src/client/api/ConnectionFactory.cs
+++ projects/client/RabbitMQ.Client/src/client/api/ConnectionFactory.cs
@@ -310,11 +310,27 @@ namespace RabbitMQ.Client
         /// <summary>
         /// Create a connection to the specified endpoint.
         /// </summary>
+        /// <exception cref="BrokerUnreachableException">
+        /// When the configured hostname was not reachable.
+        /// </exception>
         public virtual IConnection CreateConnection()
         {
             return CreateConnection(new List<string>() { HostName });
         }
 
+        /// <summary>
+        /// Create a connection using a list of hostnames. The first reachable
+        /// hostname will be used initially. Subsequent hostname picks are determined
+        /// by the <see cref="IHostnameSelector" /> configured.
+        /// </summary>
+        /// <param name="hostnames">
+        /// List of hostnames to use for the initial
+        /// connection and recovery.
+        /// </param>
+        /// <returns>Open connection</returns>
+        /// <exception cref="BrokerUnreachableException">
+        /// When no hostname was reachable.
+        /// </exception>
         public IConnection CreateConnection(IList<string> hostnames)
         {
             IConnection conn;
