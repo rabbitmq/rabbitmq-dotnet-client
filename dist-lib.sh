029350188fa40d33ff019b27126cd1e7aa4810cd
Michael Klishin <mklishin@pivotal.io>
2015-07-09 15:33:22 +0300

    Allow for versions such as 3.5.4.rc1

--- dist-lib.sh
+++ dist-lib.sh
@@ -41,8 +41,8 @@
 ##---------------------------------------------------------------------------
 
 function assembly-version {
-    local RELEASE_PATTERN="^[0-9]+(\.[0-9]+){2}$"
-    local NIGHTLY_PATTERN="^[0-9]+(\.[0-9]+){3}$"
+    local RELEASE_PATTERN="^[0-9]+(\.[0-9a-z]+){3}$"
+    local NIGHTLY_PATTERN="^[0-9]+(\.[0-9a-z]+){3}$"
     if [[ $1 =~ $RELEASE_PATTERN ]] ; then
         ASSEMBLY_VSN=$RABBIT_VSN.0
     elif [[ $1 =~ $NIGHTLY_PATTERN ]] ; then
