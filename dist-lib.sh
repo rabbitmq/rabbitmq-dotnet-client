8f815a0c5422226c329ec4fa0cee79ee87b86845
Michael Klishin <mklishin@pivotal.io>
2015-07-13 14:47:51 +0300

    Update (c) years

--- dist-lib.sh
+++ dist-lib.sh
@@ -6,7 +6,7 @@
 ## The APL v2.0:
 ##
 ##---------------------------------------------------------------------------
-##   Copyright (C) 2007-2014 GoPivotal, Inc.
+##   Copyright (C) 2007-2015 Pivotal Software, Inc.
 ##
 ##   Licensed under the Apache License, Version 2.0 (the "License");
 ##   you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@
 ##  The Original Code is RabbitMQ.
 ##
 ##  The Initial Developer of the Original Code is GoPivotal, Inc.
-##  Copyright (c) 2007-2014 GoPivotal, Inc.  All rights reserved.
+##  Copyright (c) 2007-2015 Pivotal Software, Inc.  All rights reserved.
 ##---------------------------------------------------------------------------
 
 function assembly-version {
