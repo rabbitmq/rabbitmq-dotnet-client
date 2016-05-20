98180e07d207fb537e42f6c6981d644140a367d5
kjnilsson <knilsson@pivotal.io>
2016-05-20 13:58:38 +0100

    fix run-test.bat

--- run-test.bat
+++ run-test.bat
@@ -1,8 +1,4 @@
 @ECHO OFF
-IF %0 == "" GOTO End
 .paket\paket.bootstrapper.exe
 .paket\paket.exe restore
-packages\FAKE\tools\FAKE.exe build.fsx %0
-
-:End
-ECHO "no target specified"
+packages\FAKE\tools\FAKE.exe build.fsx Test
