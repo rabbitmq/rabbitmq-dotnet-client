4cf04743256eea99b0c78c85a123981f5600b7c1
kjnilsson <knilsson@pivotal.io>
2016-06-07 11:21:42 +0100

    first dotnetcore attempt

---
+++ projects/client/testapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RabbitMQ.Client;
+namespace ConsoleApplicationTes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cf = new ConnectionFactory();
+            Console.WriteLine("Hello World!");
+            var conn = cf.CreateConnection();
+            Console.ReadLine();
+        }
+    }
+}
