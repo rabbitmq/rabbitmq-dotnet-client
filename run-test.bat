fc0a4a2c73c7bf415d9c6126e2e417d4f279d029
kjnilsson <knilsson@pivotal.io>
2016-06-14 16:43:55 +0100

    return to root directory

--- run-test.bat
+++ run-test.bat
@@ -6,3 +6,4 @@ dotnet restore .\projects\client\Unit || exit /b
 dotnet build .\projects\client\Unit || exit /b
 CD .\projects\client\Unit 
 dotnet test -f netcoreapp1.0 --where="cat != RequireSMP & cat != LongRunning & cat != GCTest"
+CD ..\..\.. 
