b91747b6f76fc4e075a2774249acf5f7dc9b88e3
kjnilsson <knilsson@pivotal.io>
2016-07-05 11:32:52 +0100

    ci script improvements

--- ci.sh
+++ ci.sh
@@ -6,20 +6,20 @@ SCRIPT=$0
 SCRIPT_DIR=$(cd $(dirname "$SCRIPT") && pwd)
 RABBIT_DIR=$SCRIPT_DIR/../rabbit
 
-cd $RABBIT_DIR
-make start-background-broker
-cd $SCRIPT_DIR
-
 CLIENT_DIR=$SCRIPT_DIR/projects/client/RabbitMQ.Client
 UNIT_DIR=$SCRIPT_DIR/projects/client/Unit
 
 dotnet restore $CLIENT_DIR
-dotnet build $CLIENT_DIR
+dotnet build -f netstandard1.5 $CLIENT_DIR
 dotnet restore $UNIT_DIR
-dotnet build $UNIT_DIR
-cd $UNIT_DIR
+dotnet build -f netcoreapp1.0 $UNIT_DIR
+
+cd $RABBIT_DIR
+make start-background-broker
 
 set +e
+
+cd $UNIT_DIR
 dotnet test -f netcoreapp1.0 --where='cat != RequireSMP & cat != LongRunning & cat != GCTest'
 EXIT_CODE=$?
 
