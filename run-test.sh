7746a74b76ceb162be55d5e72da4b36d77f1254b
kjnilsson <knilsson@pivotal.io>
2017-05-11 17:20:25 +0100

    Switch to the newold project format.
    To do this the net451 build has been removed as it currently
    isn't able to build this using mono.
    
    [#145272475]

--- run-test.sh
+++ run-test.sh
@@ -6,7 +6,10 @@ dotnet restore ./projects/client/RabbitMQ.Client
 dotnet build ./projects/client/RabbitMQ.Client
 dotnet restore ./projects/client/Unit
 dotnet build ./projects/client/Unit
+# export RABBITMQ_RABBITMQCTL_PATH=$(pwd)/../rabbit/scripts/rabbitmqctl
 cd ./projects/client/Unit
-dotnet test -f netcoreapp1.0 --where='cat != RequireSMP & cat != LongRunning & cat != GCTest'
+# dotnet test -f netcoreapp1.0 -- --where='cat != RequireSMP & cat != LongRunning & cat != GCTest'
+
+dotnet test -f netcoreapp1.0
 
 
