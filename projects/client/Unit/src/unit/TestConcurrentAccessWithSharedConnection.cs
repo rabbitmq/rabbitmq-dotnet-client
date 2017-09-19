9332cde5e7d229c59797b3866f5d7140150fd98f
Michael Klishin <mklishin@pivotal.io>
2017-09-19 16:09:57 -0600

    More concurrent connection access tests

--- projects/client/Unit/src/unit/TestConcurrentAccessWithSharedConnection.cs
+++ projects/client/Unit/src/unit/TestConcurrentAccessWithSharedConnection.cs
@@ -68,24 +68,84 @@ namespace RabbitMQ.Client.Unit {
         }
 
         [Test]
-        public void TestConcurrentChannelOpenWithPublishing()
+        public void TestConcurrentChannelOpenAndPublishingWithBlankMessages()
         {
-            TestConcurrentChannelOperations((conn) => {
-                // publishing on a shared channel is not supported
-                // and would missing the point of this test anyway
-                var ch = Conn.CreateModel();
-                ch.ConfirmSelect();
-                foreach (var j in Enumerable.Range(0, 500))
-                {
-                    var body = Encoding.ASCII.GetBytes(string.Empty);
-                    ch.BasicPublish(exchange: "", routingKey: "_______", basicProperties: ch.CreateBasicProperties(), body: body);
-                }
-                ch.WaitForConfirms(TimeSpan.FromSeconds(40));
-            }, 30);
+            TestConcurrentChannelOpenAndPublishingWithBody(Encoding.ASCII.GetBytes(string.Empty), 30);
         }
 
-        // note: refactoring this further to use an Action causes .NET Core 1.x
-        //       to segfault on OS X for no obvious reason
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase1()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(64);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase2()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(256);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase3()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(1024);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase4()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(8192);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase5()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(32768, 20);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase6()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(100000, 15);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase7()
+        {
+            // surpasses default frame size
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(131072, 12);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase8()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(270000, 10);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase9()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(540000, 6);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase10()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(1000000, 2);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase11()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(1500000, 1);
+        }
+
+        [Test]
+        public void TestConcurrentChannelOpenAndPublishingCase12()
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(128000000, 1);
+        }
+
         [Test]
         public void TestConcurrentChannelOpenCloseLoop()
         {
@@ -93,6 +153,37 @@ namespace RabbitMQ.Client.Unit {
                 var ch = conn.CreateModel();
                 ch.Close();
             }, 50);
+        }
+
+        protected void TestConcurrentChannelOpenAndPublishingWithBodyOfSize(int length)
+        {
+            TestConcurrentChannelOpenAndPublishingWithBodyOfSize(length, 30);
+        }
+
+        protected void TestConcurrentChannelOpenAndPublishingWithBodyOfSize(int length, int iterations)
+        {
+            string s = "payload";
+            if(length > s.Length)
+            {
+                s.PadRight(length);
+            }
+
+            TestConcurrentChannelOpenAndPublishingWithBody(Encoding.ASCII.GetBytes(s), iterations);
+        }
+
+        protected void TestConcurrentChannelOpenAndPublishingWithBody(byte[] body, int iterations)
+        {
+            TestConcurrentChannelOperations((conn) => {
+                // publishing on a shared channel is not supported
+                // and would missing the point of this test anyway
+                var ch = Conn.CreateModel();
+                ch.ConfirmSelect();
+                foreach (var j in Enumerable.Range(0, 200))
+                {
+                    ch.BasicPublish(exchange: "", routingKey: "_______", basicProperties: ch.CreateBasicProperties(), body: body);
+                }
+                ch.WaitForConfirms(TimeSpan.FromSeconds(40));
+            }, iterations);
         }
 
         protected void TestConcurrentChannelOperations(Action<IConnection> actions,
