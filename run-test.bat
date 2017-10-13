557be7405b16088c5820f32218802af55d364986
kjnilsson <knilsson@pivotal.io>
2017-10-13 16:52:47 +0100

    netcoreapp fixes

--- run-test.bat
+++ run-test.bat
@@ -4,6 +4,6 @@ dotnet restore .\projects\client\RabbitMQ.Client || exit /b
 dotnet build .\projects\client\RabbitMQ.Client -f netstandard1.5 || exit /b
 dotnet restore .\projects\client\Unit || exit /b
 dotnet build .\projects\client\Unit || exit /b
-CD .\projects\client\Unit 
-dotnet test -f netcoreapp1.0 --where="cat != RequireSMP & cat != LongRunning & cat != GCTest"
-CD ..\..\.. 
+CD .\projects\client\Unit
+dotnet test -f netcoreapp2.0 --where="cat != RequireSMP & cat != LongRunning & cat != GCTest"
+CD ..\..\..
