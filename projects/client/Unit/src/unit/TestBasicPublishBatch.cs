4e655c269bbd2cdfc524d216d8add20117daa89c
Michael Klishin <michael@clojurewerkz.org>
2017-12-04 13:55:13 -0800

    Use publisher confirms in this test

--- projects/client/Unit/src/unit/TestBasicPublishBatch.cs
+++ projects/client/Unit/src/unit/TestBasicPublishBatch.cs
@@ -50,12 +50,14 @@ namespace RabbitMQ.Client.Unit
         [Test]
         public void TestBasicPublishBatchSend()
         {
+            Model.ConfirmSelect();
             Model.QueueDeclare(queue: "test-message-batch-a", durable: false);
             Model.QueueDeclare(queue: "test-message-batch-b", durable: false);
             var batch = Model.CreateBasicPublishBatch();
             batch.Add("", "test-message-batch-a", false, null, new byte [] {});
             batch.Add("", "test-message-batch-b", false, null, new byte [] {});
             batch.Publish();
+            Model.WaitForConfirmsOrDie(TimeSpan.FromSeconds(15));
             var resultA = Model.BasicGet("test-message-batch-a", true);
             Assert.NotNull(resultA);
             var resultB = Model.BasicGet("test-message-batch-b", true);
