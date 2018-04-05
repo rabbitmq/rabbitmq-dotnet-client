2a7e75b97a5c27bafc162f7f5373986e99c03ddd
Psilon <pzixel@gmail.com>
2018-04-05 22:00:44 +0300

    unspecified family fix

---
+++ projects/client/RabbitMQ.Client/src/client/impl/TcpClientAdapterHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RabbitMQ.Client
+{
+    public static class TcpClientAdapterHelper
+    {
+        public static IPAddress GetMatchingHost(IReadOnlyCollection<IPAddress> addresses, AddressFamily addressFamily)
+        {
+            var ep = addresses.FirstOrDefault(a => a.AddressFamily == addressFamily);
+            if (ep == null && addresses.Count == 1 && addressFamily == AddressFamily.Unspecified)
+            {
+                return addresses.Single();
+            }
+            return ep;
+        }
+    }
+}
