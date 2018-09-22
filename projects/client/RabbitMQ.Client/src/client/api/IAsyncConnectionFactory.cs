3cc2b1548eb816a2570a7a9ade5b41d326c79363
Paulo Morgado <470455+paulomorgado@users.noreply.github.com>
2018-09-22 14:30:26 +0100

    Make IAsyncConnectionFactory

--- projects/client/RabbitMQ.Client/src/client/api/IAsyncConnectionFactory.cs
+++ projects/client/RabbitMQ.Client/src/client/api/IAsyncConnectionFactory.cs
@@ -1,7 +1,55 @@
+// This source code is dual-licensed under the Apache License, version
+// 2.0, and the Mozilla Public License, version 1.1.
+//
+// The APL v2.0:
+//
+//---------------------------------------------------------------------------
+//   Copyright (c) 2007-2016 Pivotal Software, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//---------------------------------------------------------------------------
+//
+// The MPL v1.1:
+//
+//---------------------------------------------------------------------------
+//  The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License
+//  at http://www.mozilla.org/MPL/
+//
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+//  the License for the specific language governing rights and
+//  limitations under the License.
+//
+//  The Original Code is RabbitMQ.
+//
+//  The Initial Developer of the Original Code is Pivotal Software, Inc.
+//  Copyright (c) 2007-2016 Pivotal Software, Inc.  All rights reserved.
+//---------------------------------------------------------------------------
+
 namespace RabbitMQ.Client
 {
-    internal interface IAsyncConnectionFactory : IConnectionFactory
+    /// <summary>
+    /// Defines a connection factory capable of using an asynchronous consumer dispatcher which is compatible with <see cref="IAsyncBasicConsumer"/>.
+    /// </summary>
+    /// <seealso cref="IConnectionFactory" />
+    public interface IAsyncConnectionFactory : IConnectionFactory
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether an asynchronous consumer dispatcher which is compatible with <see cref="IAsyncBasicConsumer"/> is used.
+        /// </summary>
+        /// <value><see langword="true" /> if an asynchronous consumer dispatcher which is compatible with <see cref="IAsyncBasicConsumer"/> is used; otherwise, <see langword="false" />.</value>
         bool DispatchConsumersAsync { get; set; }
     }
 }
