92919fbf81283a7ce7765153396d63461c8933b9
Spring Operator <spring-operator@users.noreply.github.com>
2019-03-15 21:03:22 -0500

    URL Cleanup
    This commit updates URLs to prefer the https protocol. Redirects are not followed to avoid accidentally expanding intentionally shortened URLs (i.e. if using a URL shortener).
    
    # Fixed URLs
    
    ## Fixed But Review Recommended
    These URLs were fixed, but the https status was not OK. However, the https status was the same as the http request or http redirected to an https URL, so they were migrated. Your review is recommended.
    
    * http://stage.rabbitmq.com/ (UnknownHostException) with 1 occurrences migrated to:
      https://stage.rabbitmq.com/ ([https](https://stage.rabbitmq.com/) result UnknownHostException).
    * http://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-api-guide.pdf (404) with 1 occurrences migrated to:
      https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-api-guide.pdf ([https](https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-api-guide.pdf) result 404).
    * http://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-user-guide.pdf (404) with 1 occurrences migrated to:
      https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-user-guide.pdf ([https](https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-user-guide.pdf) result 404).

--- tools/download-docs.sh
+++ tools/download-docs.sh
@@ -3,5 +3,5 @@
 OUTPUT_DIR=../releases/rabbitmq-dotnet-client/v1.6.0
 
 mkdir -p $OUTPUT_DIR
-(cd $OUTPUT_DIR; wget http://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-user-guide.pdf)
-(cd $OUTPUT_DIR; wget http://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-api-guide.pdf)
+(cd $OUTPUT_DIR; wget https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-user-guide.pdf)
+(cd $OUTPUT_DIR; wget https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-api-guide.pdf)
