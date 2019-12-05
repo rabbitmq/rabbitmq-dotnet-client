89630cf75a5c68dd0a01f732c91beead808d6f39
Brandon Ording <bording@gmail.com>
2019-12-05 13:33:32 -0500

    Update test scripts

--- run-test.bat
+++ run-test.bat
@@ -1,8 +1,4 @@
 @echo off
-dotnet restore .\projects\client\RabbitMQ.Client || exit /b
-dotnet build .\projects\client\RabbitMQ.Client -f netstandard1.5 || exit /b
-dotnet restore .\projects\client\Unit || exit /b
-dotnet build .\projects\client\Unit || exit /b
 cd .\projects\client\Unit
-dotnet test -f netcoreapp2.0 --filter="testcategory != requiresmp & testcategory != longrunning & testcategory != gctest"
+dotnet test -f netcoreapp2.1 --filter="testcategory != requiresmp & testcategory != longrunning & testcategory != gctest"
 cd ..\..\..
