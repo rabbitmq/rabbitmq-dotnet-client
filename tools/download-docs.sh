b84063e0a1f1c0007f2c248eeff11fac242c8f53
Luke Bakken <lbakken@pivotal.io>
2020-02-25 09:30:36 -0800

    Remove unused files

--- tools/download-docs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-OUTPUT_DIR=../releases/rabbitmq-dotnet-client/v1.6.0
-
-mkdir -p $OUTPUT_DIR
-(cd $OUTPUT_DIR; wget https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-user-guide.pdf)
-(cd $OUTPUT_DIR; wget https://www.rabbitmq.com/releases/rabbitmq-dotnet-client/v1.6.0/rabbitmq-dotnet-client-1.6.0-api-guide.pdf)
