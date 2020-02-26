e9390a0ad726346611f6390aa07de226f269179a
Stefán Jökull Sigurðarson <stefanjokull@hotmail.com>
2020-02-26 10:38:05 +0000

    Removing unused code.

--- projects/client/RabbitMQ.Client/src/client/impl/WireFormatting.cs
+++ projects/client/RabbitMQ.Client/src/client/impl/WireFormatting.cs
@@ -95,20 +95,6 @@ namespace RabbitMQ.Client.Impl
                              (((uint)bitRepresentation[0]) & 0x7FFFFFFF));
         }
 
-        public static IList ReadArray(NetworkBinaryReader reader)
-        {
-            IList array = new List<object>();
-            long arrayLength = reader.ReadUInt32();
-            Stream backingStream = reader.BaseStream;
-            long startPosition = backingStream.Position;
-            while ((backingStream.Position - startPosition) < arrayLength)
-            {
-                object value = ReadFieldValue(reader);
-                array.Add(value);
-            }
-            return array;
-        }
-
         public static IList ReadArray(Memory<byte> memory, out int bytesRead)
         {
             IList array = new List<object>();
@@ -124,59 +110,11 @@ namespace RabbitMQ.Client.Impl
             return array;
         }
 
-        public static decimal ReadDecimal(NetworkBinaryReader reader)
-        {
-            byte scale = ReadOctet(reader);
-            uint unsignedMantissa = ReadLong(reader);
-            return AmqpToDecimal(scale, unsignedMantissa);
-        }
-
         public static decimal ReadDecimal(Memory<byte> memory)
         {
             byte scale = memory.Span[0];
             uint unsignedMantissa = NetworkOrderDeserializer.ReadUInt32(memory.Slice(1));
             return AmqpToDecimal(scale, unsignedMantissa);
-        }
-
-        public static object ReadFieldValue(NetworkBinaryReader reader)
-        {
-            switch ((char)reader.ReadByte())
-            {
-                case 'S':
-                    return ReadLongstr(reader);
-                case 'I':
-                    return reader.ReadInt32();
-                case 'i':
-                    return reader.ReadUInt32();
-                case 'D':
-                    return ReadDecimal(reader);
-                case 'T':
-                    return ReadTimestamp(reader);
-                case 'F':
-                    return ReadTable(reader);
-                case 'A':
-                    return ReadArray(reader);
-                case 'B':
-                    return reader.ReadByte();
-                case 'b':
-                    return reader.ReadSByte();
-                case 'd':
-                    return reader.ReadDouble();
-                case 'f':
-                    return reader.ReadSingle();
-                case 'l':
-                    return reader.ReadInt64();
-                case 's':
-                    return reader.ReadInt16();
-                case 't':
-                    return ReadOctet(reader) != 0;
-                case 'x':
-                    return new BinaryTableValue(ReadLongstr(reader));
-                case 'V':
-                    return null;
-                default:
-                    throw new SyntaxError($"Unrecognised type in table: {(char)reader.ReadByte()}");
-            }
         }
 
         public static object ReadFieldValue(Memory<byte> memory, out int bytesRead)
@@ -241,27 +179,6 @@ namespace RabbitMQ.Client.Impl
             }
         }
 
-        public static uint ReadLong(NetworkBinaryReader reader)
-        {
-            return reader.ReadUInt32();
-        }
-
-        public static ulong ReadLonglong(NetworkBinaryReader reader)
-        {
-            return reader.ReadUInt64();
-        }
-
-        public static byte[] ReadLongstr(NetworkBinaryReader reader)
-        {
-            uint byteCount = reader.ReadUInt32();
-            if (byteCount > int.MaxValue)
-            {
-                throw new SyntaxError("Long string too long; " +
-                                      "byte length=" + byteCount + ", max=" + int.MaxValue);
-            }
-            return reader.ReadBytes((int)byteCount);
-        }
-
         public static byte[] ReadLongstr(Memory<byte> memory)
         {
             int byteCount = (int)NetworkOrderDeserializer.ReadUInt32(memory);
@@ -271,23 +188,6 @@ namespace RabbitMQ.Client.Impl
             }
 
             return memory.Slice(4, byteCount).ToArray();
-        }
-
-        public static byte ReadOctet(NetworkBinaryReader reader)
-        {
-            return reader.ReadByte();
-        }
-
-        public static ushort ReadShort(NetworkBinaryReader reader)
-        {
-            return reader.ReadUInt16();
-        }
-
-        public static string ReadShortstr(NetworkBinaryReader reader)
-        {
-            int byteCount = reader.ReadByte();
-            byte[] readBytes = reader.ReadBytes(byteCount);
-            return Encoding.UTF8.GetString(readBytes, 0, readBytes.Length);
         }
 
         public static string ReadShortstr(Memory<byte> memory, out int bytesRead)
@@ -310,27 +210,6 @@ namespace RabbitMQ.Client.Impl
         /// x and V types and the AMQP 0-9-1 A type.
         ///</remarks>
         /// <returns>A <seealso cref="System.Collections.Generic.IDictionary{TKey,TValue}"/>.</returns>
-        public static IDictionary<string, object> ReadTable(NetworkBinaryReader reader)
-        {
-            IDictionary<string, object> table = new Dictionary<string, object>();
-            long tableLength = reader.ReadUInt32();
-
-            Stream backingStream = reader.BaseStream;
-            long startPosition = backingStream.Position;
-            while ((backingStream.Position - startPosition) < tableLength)
-            {
-                string key = ReadShortstr(reader);
-                object value = ReadFieldValue(reader);
-
-                if (!table.ContainsKey(key))
-                {
-                    table[key] = value;
-                }
-            }
-
-            return table;
-        }
-
         public static IDictionary<string, object> ReadTable(Memory<byte> memory, out int bytesRead)
         {
             IDictionary<string, object> table = new Dictionary<string, object>();
@@ -352,43 +231,12 @@ namespace RabbitMQ.Client.Impl
             return table;
         }
 
-        public static AmqpTimestamp ReadTimestamp(NetworkBinaryReader reader)
-        {
-            ulong stamp = ReadLonglong(reader);
-            // 0-9 is afaict silent on the signedness of the timestamp.
-            // See also MethodArgumentWriter.WriteTimestamp and AmqpTimestamp itself
-            return new AmqpTimestamp((long)stamp);
-        }
-
         public static AmqpTimestamp ReadTimestamp(Memory<byte> memory)
         {
             ulong stamp = NetworkOrderDeserializer.ReadUInt64(memory);
             // 0-9 is afaict silent on the signedness of the timestamp.
             // See also MethodArgumentWriter.WriteTimestamp and AmqpTimestamp itself
             return new AmqpTimestamp((long)stamp);
-        }
-
-        public static void WriteArray(NetworkBinaryWriter writer, IList val)
-        {
-            if (val == null)
-            {
-                writer.Write((uint)0);
-            }
-            else
-            {
-                Stream backingStream = writer.BaseStream;
-                long patchPosition = backingStream.Position;
-                writer.Write((uint)0); // length of table - will be backpatched
-                foreach (object entry in val)
-                {
-                    WriteFieldValue(writer, entry);
-                }
-                long savedPosition = backingStream.Position;
-                long tableLength = savedPosition - patchPosition - 4; // offset for length word
-                backingStream.Seek(patchPosition, SeekOrigin.Begin);
-                writer.Write((uint)tableLength);
-                backingStream.Seek(savedPosition, SeekOrigin.Begin);
-            }
         }
 
         public static int WriteArray(Memory<byte> memory, IList val)
@@ -427,106 +275,11 @@ namespace RabbitMQ.Client.Impl
             return byteCount;
         }
 
-        public static void WriteDecimal(NetworkBinaryWriter writer, decimal value)
-        {
-            DecimalToAmqp(value, out byte scale, out int mantissa);
-            WriteOctet(writer, scale);
-            WriteLong(writer, (uint)mantissa);
-        }
-
         public static int WriteDecimal(Memory<byte> memory, decimal value)
         {
             DecimalToAmqp(value, out byte scale, out int mantissa);
             memory.Span[0] = scale;
             return 1 + WriteLong(memory.Slice(1), (uint)mantissa);
-        }
-
-        public static void WriteFieldValue(NetworkBinaryWriter writer, object value)
-        {
-            switch (value)
-            {
-                case null:
-                    WriteOctet(writer, (byte)'V');
-                    break;
-                case string val:
-                    WriteOctet(writer, (byte)'S');
-                    int maxLength = Encoding.UTF8.GetByteCount(val);
-                    byte[] bytes = ArrayPool<byte>.Shared.Rent(maxLength);
-                    try
-                    {
-                        int bytesUsed = Encoding.UTF8.GetBytes(val, 0, val.Length, bytes, 0);
-                        WriteLongstr(writer, bytes, 0, bytesUsed);
-                    }
-                    finally
-                    {
-                        ArrayPool<byte>.Shared.Return(bytes);
-                    }
-                    break;
-                case byte[] val:
-                    WriteOctet(writer, (byte)'S');
-                    WriteLongstr(writer, val);
-                    break;
-                case int val:
-                    WriteOctet(writer, (byte)'I');
-                    writer.Write(val);
-                    break;
-                case uint val:
-                    WriteOctet(writer, (byte)'i');
-                    writer.Write(val);
-                    break;
-                case decimal val:
-                    WriteOctet(writer, (byte)'D');
-                    WriteDecimal(writer, val);
-                    break;
-                case AmqpTimestamp val:
-                    WriteOctet(writer, (byte)'T');
-                    WriteTimestamp(writer, val);
-                    break;
-                case IDictionary val:
-                    WriteOctet(writer, (byte)'F');
-                    WriteTable(writer, val);
-                    break;
-                case IList val:
-                    WriteOctet(writer, (byte)'A');
-                    WriteArray(writer, val);
-                    break;
-                case byte val:
-                    WriteOctet(writer, (byte)'B');
-                    writer.Write(val);
-                    break;
-                case sbyte val:
-                    WriteOctet(writer, (byte)'b');
-                    writer.Write(val);
-                    break;
-                case double val:
-                    WriteOctet(writer, (byte)'d');
-                    writer.Write(val);
-                    break;
-                case float val:
-                    WriteOctet(writer, (byte)'f');
-                    writer.Write(val);
-                    break;
-                case long val:
-                    WriteOctet(writer, (byte)'l');
-                    writer.Write(val);
-                    break;
-                case short val:
-                    WriteOctet(writer, (byte)'s');
-                    writer.Write(val);
-                    break;
-                case bool val:
-                    WriteOctet(writer, (byte)'t');
-                    WriteOctet(writer, (byte)(val ? 1 : 0));
-                    break;
-                case BinaryTableValue val:
-                    WriteOctet(writer, (byte)'x');
-                    WriteLongstr(writer, val.Bytes);
-                    break;
-                default:
-                    throw new WireFormattingException(
-                        $"Value of type '{value.GetType().Name}' cannot appear as table value",
-                        value);
-            }
         }
 
         public static int WriteFieldValue(Memory<byte> memory, object value)
@@ -646,20 +399,10 @@ namespace RabbitMQ.Client.Impl
             }
         }
 
-        public static void WriteLong(NetworkBinaryWriter writer, uint val)
-        {
-            writer.Write(val);
-        }
-
         public static int WriteLong(Memory<byte> memory, uint val)
         {
             NetworkOrderSerializer.WriteUInt32(memory, val);
             return 4;
-        }
-
-        public static void WriteLonglong(NetworkBinaryWriter writer, ulong val)
-        {
-            writer.Write(val);
         }
 
         public static int WriteLonglong(Memory<byte> memory, ulong val)
@@ -668,21 +411,9 @@ namespace RabbitMQ.Client.Impl
             return 8;
         }
 
-        public static void WriteLongstr(NetworkBinaryWriter writer, byte[] val)
-        {
-            WriteLong(writer, (uint)val.Length);
-            writer.Write(val);
-        }
-
         public static int WriteLongstr(Memory<byte> memory, byte[] val)
         {
             return WriteLongstr(memory, val, 0, val.Length);
-        }
-
-        public static void WriteLongstr(NetworkBinaryWriter writer, byte[] val, int index, int count)
-        {
-            WriteLong(writer, (uint)count);
-            writer.Write(val, index, count);
         }
 
         public static int WriteLongstr(Memory<byte> memory, byte[] val, int index, int count)
@@ -692,41 +423,10 @@ namespace RabbitMQ.Client.Impl
             return 4 + count;
         }
 
-        public static void WriteOctet(NetworkBinaryWriter writer, byte val)
-        {
-            writer.Write(val);
-        }
-
-        public static void WriteShort(NetworkBinaryWriter writer, ushort val)
-        {
-            writer.Write(val);
-        }
-
         public static int WriteShort(Memory<byte> memory, ushort val)
         {
             NetworkOrderSerializer.WriteUInt16(memory, val);
             return 2;
-        }
-
-        public static void WriteShortstr(NetworkBinaryWriter writer, string val)
-        {
-            int maxLength = Encoding.UTF8.GetByteCount(val);
-            byte[] bytes = ArrayPool<byte>.Shared.Rent(maxLength);
-            try
-            {
-                int bytesUsed = Encoding.UTF8.GetBytes(val, 0, val.Length, bytes, 0);
-                if (bytesUsed > 255)
-                {
-                    throw new WireFormattingException($"Short string too long; UTF-8 encoded length={bytesUsed}, max=255");
-                }
-
-                writer.Write((byte)bytesUsed);
-                writer.Write(bytes, 0, bytesUsed);
-            }
-            finally
-            {
-                ArrayPool<byte>.Shared.Return(bytes);
-            }
         }
 
         public static int WriteShortstr(Memory<byte> memory, string val)
@@ -740,47 +440,6 @@ namespace RabbitMQ.Client.Impl
             }
 
             throw new WireFormattingException("Unable to get array segment from memory.");
-        }
-
-        ///<summary>Writes an AMQP "table" to the writer.</summary>
-        ///<remarks>
-        ///<para>
-        /// In this method, we assume that the stream that backs our
-        /// NetworkBinaryWriter is a positionable stream - which it is
-        /// currently (see Frame.m_accumulator, Frame.GetWriter and
-        /// Command.Transmit).
-        ///</para>
-        ///<para>
-        /// Supports the AMQP 0-8/0-9 standard entry types S, I, D, T
-        /// and F, as well as the QPid-0-8 specific b, d, f, l, s, t
-        /// x and V types and the AMQP 0-9-1 A type.
-        ///</para>
-        ///</remarks>
-        public static void WriteTable(NetworkBinaryWriter writer, IDictionary val)
-        {
-            if (val == null)
-            {
-                writer.Write((uint)0);
-            }
-            else
-            {
-                Stream backingStream = writer.BaseStream;
-                long patchPosition = backingStream.Position;
-                writer.Write((uint)0); // length of table - will be backpatched
-
-                foreach (DictionaryEntry entry in val)
-                {
-                    WriteShortstr(writer, entry.Key.ToString());
-                    WriteFieldValue(writer, entry.Value);
-                }
-
-                // Now, backpatch the table length.
-                long savedPosition = backingStream.Position;
-                long tableLength = savedPosition - patchPosition - 4; // offset for length word
-                backingStream.Seek(patchPosition, SeekOrigin.Begin);
-                writer.Write((uint)tableLength);
-                backingStream.Seek(savedPosition, SeekOrigin.Begin);
-            }
         }
 
         public static int WriteTable(Memory<byte> memory, IDictionary val)
@@ -861,54 +520,6 @@ namespace RabbitMQ.Client.Impl
             }
 
             return byteCount;
-        }
-
-        ///<summary>Writes an AMQP "table" to the writer.</summary>
-        ///<remarks>
-        ///<para>
-        /// In this method, we assume that the stream that backs our
-        /// NetworkBinaryWriter is a positionable stream - which it is
-        /// currently (see Frame.m_accumulator, Frame.GetWriter and
-        /// Command.Transmit).
-        ///</para>
-        ///<para>
-        /// Supports the AMQP 0-8/0-9 standard entry types S, I, D, T
-        /// and F, as well as the QPid-0-8 specific b, d, f, l, s, t
-        /// x and V types and the AMQP 0-9-1 A type.
-        ///</para>
-        ///</remarks>
-        public static void WriteTable(NetworkBinaryWriter writer, IDictionary<string, object> val)
-        {
-            if (val == null)
-            {
-                writer.Write((uint)0);
-            }
-            else
-            {
-                Stream backingStream = writer.BaseStream;
-                long patchPosition = backingStream.Position;
-                writer.Write((uint)0); // length of table - will be backpatched
-
-                foreach (KeyValuePair<string, object> entry in val)
-                {
-                    WriteShortstr(writer, entry.Key);
-                    WriteFieldValue(writer, entry.Value);
-                }
-
-                // Now, backpatch the table length.
-                long savedPosition = backingStream.Position;
-                long tableLength = savedPosition - patchPosition - 4; // offset for length word
-                backingStream.Seek(patchPosition, SeekOrigin.Begin);
-                writer.Write((uint)tableLength);
-                backingStream.Seek(savedPosition, SeekOrigin.Begin);
-            }
-        }
-
-        public static void WriteTimestamp(NetworkBinaryWriter writer, AmqpTimestamp val)
-        {
-            // 0-9 is afaict silent on the signedness of the timestamp.
-            // See also MethodArgumentReader.ReadTimestamp and AmqpTimestamp itself
-            WriteLonglong(writer, (ulong)val.UnixTime);
         }
 
         public static int WriteTimestamp(Memory<byte> memory, AmqpTimestamp val)
