5e6a651902e5db226067e1ed82e0666b99f1dc16
Luke Bakken <lbakken@pivotal.io>
2020-03-04 10:44:42 -0800

    Merge pull request #750 from rabbitmq/lrb-travis-ci
    Add Travis CI build
    
    (cherry picked from commit a469b71195ad95e160b646972e5c438aef8a6526)
    
    Change .NET core version

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
+
 set -e
-dotnet test --no-build --logger 'console;verbosity=detailed' ./RabbitMQDotNetClient.sln
+
+if command -v realpath >/dev/null 2>&1
+then
+    readonly script_dir="$(dirname "$(realpath "$0")")"
+else
+    readonly script_dir="$(cd "$(dirname "$0")" && pwd)"
+fi
+
+cd "$script_dir"
+
+dotnet test --no-build --logger 'console;verbosity=detailed' ./RabbitMQDotNetClient.sln < /dev/null
