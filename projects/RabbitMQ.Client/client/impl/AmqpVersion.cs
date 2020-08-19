20486e3a06537bf4f4b27901744ee12e9027aa16
Michael Klishin <michael@clojurewerkz.org>
2020-08-19 04:11:38 +0300

    Roll back the hash code suggestion that requires an extra dependency

--- projects/RabbitMQ.Client/client/impl/AmqpVersion.cs
+++ projects/RabbitMQ.Client/client/impl/AmqpVersion.cs
@@ -100,7 +100,7 @@ namespace RabbitMQ.Client.Framing.Impl
         {
             unchecked
             {
-                return (Major, Minor).GetHashCode();
+                return (Major * 397) ^ Minor;
             }
         }
 
