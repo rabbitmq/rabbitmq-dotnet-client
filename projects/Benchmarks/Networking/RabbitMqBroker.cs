99c534fc751e167d8786ce9cac656ec1ac07a9b5
JanEggers <jan.eggers@ferag.com>
2020-10-16 21:56:14 +0200

    added basic networking benchmarks

---
+++ projects/Benchmarks/Networking/RabbitMqBroker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Ductus.FluentDocker.Builders;
+
+namespace Benchmarks.Networking
+{
+    public class RabbitMqBroker
+    {
+        public static IDisposable Start()
+        {
+            var broker = new Builder().UseContainer()
+               .UseImage("rabbitmq")
+               .ExposePort(5672, 5672)
+               .WaitForPort("5672/tcp", 40000 /*40s*/)
+               .ReuseIfExists()
+               .WithName("rabbitmq")
+               .Build()
+               .Start();
+
+            Thread.Sleep(5000); // the broker needs some more time after the port is open to be operational
+
+            return broker;
+        }
+    }
+}
