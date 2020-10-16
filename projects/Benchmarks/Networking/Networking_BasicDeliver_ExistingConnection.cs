2f2880661bda9ffce6ded6c7bc50507c423e8aaf
JanEggers <jan.eggers@ferag.com>
2020-10-17 00:13:45 +0200

    refactoring

--- projects/Benchmarks/Networking/Networking_BasicDeliver_ExistingConnection.cs
+++ projects/Benchmarks/Networking/Networking_BasicDeliver_ExistingConnection.cs
@@ -13,54 +13,30 @@ namespace Benchmarks.Networking
     {
         private int messageCount = 10000;
 
-        private IDisposable container;
-        private IConnection connection;
+        private IDisposable _container;
+        private IConnection _connection;
 
         [GlobalSetup]
         public void GlobalSetup()
         {
-            container = RabbitMQBroker.Start();
+            _container = RabbitMQBroker.Start();
 
             var cf = new ConnectionFactory { ConsumerDispatchConcurrency = 2 };
-            connection = cf.CreateConnection();
+            _connection = cf.CreateConnection();
         }
 
         [GlobalCleanup]
         public void GlobalCleanup()
         {
-            container.Dispose();
-            connection.Dispose();
+            _container.Dispose();
+            _connection.Dispose();
         }
 
 
         [Benchmark(Baseline = true)]
         public async Task Publish_Hello_World()
         {
-            var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
-            using (var model = connection.CreateModel())
-            {
-                var queue = model.QueueDeclare();
-                var consumed = 0;
-                var consumer = new EventingBasicConsumer(model);
-                consumer.Received += (s, args) =>
-                {
-                    if (Interlocked.Increment(ref consumed) == messageCount)
-                    {
-                        tcs.SetResult(true);
-                    }
-                };
-                model.BasicConsume(queue.QueueName, true, consumer);
-
-                const string publish1 = "hello world";
-                byte[] body = Encoding.UTF8.GetBytes(publish1);
-                for (int i = 0; i < messageCount; i++)
-                {
-                    var bp = model.CreateBasicProperties();
-                    model.BasicPublish("", queue.QueueName, bp, body);
-                }
-
-                await tcs.Task;
-            }
+            await Networking_BasicDeliver.Publish_Hello_World(_connection);
         }
     }
 }
