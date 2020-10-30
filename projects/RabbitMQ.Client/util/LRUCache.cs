e3ac8cd9d3021992fd8b45876445fe62208910f8
Stefán J. Sigurðarson <stefanj@ccpgames.com>
2020-10-30 17:14:03 +0000

    Test adding an LRU cache for interned strings.

---
+++ projects/RabbitMQ.Client/util/LRUCache.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace RabbitMQ.Client.util
+{
+    internal class LRUCache<K, V>
+    {
+        private int capacity;
+        private readonly ConcurrentDictionary<K, LinkedListNode<LRUCacheItem<K, V>>> _cacheMap = new ConcurrentDictionary<K, LinkedListNode<LRUCacheItem<K, V>>>();
+        private readonly LinkedList<LRUCacheItem<K, V>> _lruList = new LinkedList<LRUCacheItem<K, V>>();
+
+        public LRUCache(int capacity)
+        {
+            this.capacity = capacity;
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public V get(K key)
+        {
+            LinkedListNode<LRUCacheItem<K, V>> node;
+            if (_cacheMap.TryGetValue(key, out node))
+            {
+                V value = node.Value.value;
+                _lruList.Remove(node);
+                _lruList.AddLast(node);
+                return value;
+            }
+            return default(V);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void add(K key, V val)
+        {
+            if (_cacheMap.Count >= capacity)
+            {
+                RemoveFirst();
+            }
+
+            LRUCacheItem<K, V> cacheItem = new LRUCacheItem<K, V>(key, val);
+            LinkedListNode<LRUCacheItem<K, V>> node = new LinkedListNode<LRUCacheItem<K, V>>(cacheItem);
+            _lruList.AddLast(node);
+            _cacheMap.TryAdd(key, node);
+        }
+
+        private void RemoveFirst()
+        {
+            // Remove from LRUPriority
+            LinkedListNode<LRUCacheItem<K, V>> node = _lruList.First;
+            _lruList.RemoveFirst();
+
+            // Remove from cache
+            _cacheMap.TryRemove(node.Value.key, out _);
+        }
+    }
+
+    class LRUCacheItem<K, V>
+    {
+        public LRUCacheItem(K k, V v)
+        {
+            key = k;
+            value = v;
+        }
+        public K key;
+        public V value;
+    }
+}
