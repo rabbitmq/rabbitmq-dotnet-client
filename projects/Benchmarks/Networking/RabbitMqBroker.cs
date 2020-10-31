48dc6ac6b23e82e782b2f3cfa302f42e94b56017
JanEggers <jan.eggers@ferag.com>
2020-10-31 07:23:01 +0100

    fixed waiting on startup

--- projects/Benchmarks/Networking/RabbitMqBroker.cs
+++ projects/Benchmarks/Networking/RabbitMqBroker.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Extensions;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Benchmarks.Networking
 {
-    public class RabbitMQBroker
+    public class RabbitMQBroker : IDisposable
     {
+        private readonly IContainerService _service;
+
         public static IDisposable Start()
         {
             var broker = new Builder().UseContainer()
@@ -15,11 +21,31 @@ namespace Benchmarks.Networking
                .ReuseIfExists()
                .WithName("rabbitmq")
                .WithEnvironment("NODENAME=rabbit1")
-               .ExecuteOnRunning("rabbitmqctl --node rabbit1 await_startup ")
                .Build()
                .Start();
 
-            return broker;
+            for (int i = 0; i < 10; i++)
+            {
+                var response = broker.Execute("rabbitmqctl--node rabbit1 await_startup ");
+                if (response.Success)
+                {
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
+
+            return new RabbitMQBroker(broker);
+        }
+
+        public RabbitMQBroker(IContainerService service)
+        {
+            _service = service;
+        }
+
+        public void Dispose()
+        {
+            _service?.Dispose();
         }
     }
 }
