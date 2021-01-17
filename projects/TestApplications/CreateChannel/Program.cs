98fb6c300c8856af0912c6693cf6de4ee50c9a2a
bollhals <boli89@gmail.com>
2021-01-18 00:11:23 +0100

    add two test applications

---
+++ projects/TestApplications/CreateChannel/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace CreateChannel
+{
+    public static class Program
+    {
+        private const int Repeats = 100;
+        private const int ChannelsToOpen = 50;
+
+        private static int channelsOpened;
+        private static AutoResetEvent doneEvent;
+
+        public static void Main()
+        {
+            ThreadPool.SetMinThreads(16 * Environment.ProcessorCount, 16 * Environment.ProcessorCount);
+
+            doneEvent = new AutoResetEvent(false);
+
+            var connectionFactory = new ConnectionFactory { DispatchConsumersAsync = true };
+            var connection = connectionFactory.CreateConnection();
+
+            var watch = Stopwatch.StartNew();
+            _ = Task.Run(() =>
+            {
+                var channels = new IModel[ChannelsToOpen];
+                for (int i = 0; i < Repeats; i++)
+                {
+                    for (int j = 0; j < channels.Length; j++)
+                    {
+                        channels[j] = connection.CreateModel();
+                        channelsOpened++;
+                    }
+
+                    for (int j = 0; j < channels.Length; j++)
+                    {
+                        channels[j].Dispose();
+                    }
+                }
+
+                doneEvent.Set();
+            });
+
+            Console.WriteLine($"{Repeats} times opening {ChannelsToOpen} channels on a connection. => Total channel open/close: {Repeats * ChannelsToOpen}");
+            Console.WriteLine();
+            Console.WriteLine("Opened");
+            while (!doneEvent.WaitOne(500))
+            {
+                Console.WriteLine($"{channelsOpened,5}");
+            }
+            watch.Stop();
+            Console.WriteLine($"{channelsOpened,5}");
+            Console.WriteLine();
+            Console.WriteLine($"Took {watch.Elapsed.TotalMilliseconds} ms");
+
+            connection.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
