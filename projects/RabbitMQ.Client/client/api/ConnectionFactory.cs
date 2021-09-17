37156e5dcb92f76375039e543e0d7812ee34d343
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-09-17 08:09:07 +0800

    Correct a comment for ConnectionFactory

--- projects/RabbitMQ.Client/client/api/ConnectionFactory.cs
+++ projects/RabbitMQ.Client/client/api/ConnectionFactory.cs
@@ -51,7 +51,7 @@ namespace RabbitMQ.Client
     /// <example><code>
     ///     ConnectionFactory factory = new ConnectionFactory();
     ///     //
-    ///     // The next six lines are optional:
+    ///     // The next five lines are optional:
     ///     factory.UserName = ConnectionFactory.DefaultUser;
     ///     factory.Password = ConnectionFactory.DefaultPass;
     ///     factory.VirtualHost = ConnectionFactory.DefaultVHost;
