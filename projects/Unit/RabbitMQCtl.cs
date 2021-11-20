492ea4526078a5e10633e3be38ce565a818c3edc
bollhals <boli89@gmail.com>
2021-11-20 01:01:43 +0100

    simplify code around RabbitMQCtl

--- projects/Unit/RabbitMQCtl.cs
+++ projects/Unit/RabbitMQCtl.cs
@@ -32,100 +32,83 @@
 #pragma warning disable 2002
 
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace RabbitMQ.Client.Unit
 {
+    #nullable enable
     public static class RabbitMQCtl
     {
-        //
-        // Shelling Out
-        //
-        private static Process ExecRabbitMQCtl(string args)
+        private static readonly Func<string, Process> s_invokeRabbitMqCtl = GetRabbitMqCtlInvokeAction();
+
+        private static Func<string, Process> GetRabbitMqCtlInvokeAction()
         {
-            // Allow the path to the rabbitmqctl.bat to be set per machine
-            string envVariable = Environment.GetEnvironmentVariable("RABBITMQ_RABBITMQCTL_PATH");
-            string rabbitmqctlPath;
+            string precomputedArguments;
+            string? envVariable = Environment.GetEnvironmentVariable("RABBITMQ_RABBITMQCTL_PATH");
 
-            if (envVariable != null)
+            if (envVariable is not null)
             {
-                var regex = new Regex(@"^DOCKER:(?<dockerMachine>.+)$");
-                Match match = regex.Match(envVariable);
+                const string DockerPrefix = "DOCKER:";
+                if (envVariable.StartsWith(DockerPrefix))
+                {
+                    // Call docker
+                    precomputedArguments = $"exec {envVariable.Substring(DockerPrefix.Length)} rabbitmqctl ";
+                    return args => CreateProcess("docker", precomputedArguments + args);
+                }
 
-                if (match.Success)
-                {
-                    return ExecRabbitMQCtlUsingDocker(args, match.Groups["dockerMachine"].Value);
-                }
-                else
-                {
-                    rabbitmqctlPath = envVariable;
-                }
+                // call the path from the env var
+                return args => CreateProcess(envVariable, args);
+            }
+
+            // Try default
+            string umbrellaRabbitmqctlPath;
+            string providedRabbitmqctlPath;
+
+            if (IsRunningOnMonoOrDotNetCore())
+            {
+                umbrellaRabbitmqctlPath = "../../../../../../rabbit/scripts/rabbitmqctl";
+                providedRabbitmqctlPath = "rabbitmqctl";
             }
             else
             {
-                // provided by the umbrella
-                string umbrellaRabbitmqctlPath;
-                // provided in PATH by a RabbitMQ installation
-                string providedRabbitmqctlPath;
-
-                if (IsRunningOnMonoOrDotNetCore())
-                {
-                    umbrellaRabbitmqctlPath = "../../../../../../rabbit/scripts/rabbitmqctl";
-                    providedRabbitmqctlPath = "rabbitmqctl";
-                }
-                else
-                {
-                    umbrellaRabbitmqctlPath = @"..\..\..\..\..\..\rabbit\scripts\rabbitmqctl.bat";
-                    providedRabbitmqctlPath = "rabbitmqctl.bat";
-                }
-
-                if (File.Exists(umbrellaRabbitmqctlPath))
-                {
-                    rabbitmqctlPath = umbrellaRabbitmqctlPath;
-                }
-                else
-                {
-                    rabbitmqctlPath = providedRabbitmqctlPath;
-                }
+                umbrellaRabbitmqctlPath = @"..\..\..\..\..\..\rabbit\scripts\rabbitmqctl.bat";
+                providedRabbitmqctlPath = "rabbitmqctl.bat";
             }
 
-            return ExecCommand(rabbitmqctlPath, args);
+            string path = File.Exists(umbrellaRabbitmqctlPath) ? umbrellaRabbitmqctlPath : providedRabbitmqctlPath;
+
+            if (IsRunningOnMonoOrDotNetCore())
+            {
+                return args => CreateProcess(path, args);
+            }
+
+            precomputedArguments = $"/c \"\"{path}\" ";
+            return args => CreateProcess("cmd.exe", precomputedArguments + args);
         }
 
-        private static Process ExecRabbitMQCtlUsingDocker(string args, string dockerMachineName)
+        //
+        // Shelling Out
+        //
+        private static string ExecRabbitMQCtl(string args)
         {
-            var proc = new Process
-            {
-                StartInfo =
-                {
-                    CreateNoWindow = true,
-                    UseShellExecute = false
-                }
-            };
-
             try
             {
-                proc.StartInfo.FileName = "docker";
-                proc.StartInfo.Arguments = $"exec {dockerMachineName} rabbitmqctl {args}";
-                proc.StartInfo.RedirectStandardError = true;
-                proc.StartInfo.RedirectStandardOutput = true;
+                using var process = s_invokeRabbitMqCtl(args);
+                process.Start();
+                process.WaitForExit();
+                string stderr = process.StandardError.ReadToEnd();
+                string stdout = process.StandardOutput.ReadToEnd();
 
-                proc.Start();
-                string stderr = proc.StandardError.ReadToEnd();
-                proc.WaitForExit();
-                if (stderr.Length > 0 || proc.ExitCode > 0)
+                if (stderr.Length > 0 || process.ExitCode > 0)
                 {
-                    string stdout = proc.StandardOutput.ReadToEnd();
                     ReportExecFailure("rabbitmqctl", args, $"{stderr}\n{stdout}");
                 }
 
-                return proc;
+                return stdout;
             }
             catch (Exception e)
             {
@@ -134,60 +117,21 @@ namespace RabbitMQ.Client.Unit
             }
         }
 
-        private static Process ExecCommand(string command, string args)
+        private static Process CreateProcess(string cmd,  string arguments, string? workDirectory = null)
         {
-            return ExecCommand(command, args, null);
-        }
-
-        private static Process ExecCommand(string ctl, string args, string changeDirTo)
-        {
-            var proc = new Process
+            return new Process
             {
                 StartInfo =
                 {
                     CreateNoWindow = true,
-                    UseShellExecute = false
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    FileName = cmd,
+                    Arguments = arguments,
+                    WorkingDirectory = workDirectory
                 }
             };
-            if (changeDirTo != null)
-            {
-                proc.StartInfo.WorkingDirectory = changeDirTo;
-            }
-
-            string cmd;
-            if (IsRunningOnMonoOrDotNetCore())
-            {
-                cmd = ctl;
-            }
-            else
-            {
-                cmd = "cmd.exe";
-                args = $"/c \"\"{ctl}\" {args}\"";
-            }
-
-            try
-            {
-                proc.StartInfo.FileName = cmd;
-                proc.StartInfo.Arguments = args;
-                proc.StartInfo.RedirectStandardError = true;
-                proc.StartInfo.RedirectStandardOutput = true;
-
-                proc.Start();
-                string stderr = proc.StandardError.ReadToEnd();
-                proc.WaitForExit();
-                if (stderr.Length > 0 || proc.ExitCode > 0)
-                {
-                    string stdout = proc.StandardOutput.ReadToEnd();
-                    ReportExecFailure(cmd, args, $"{stderr}\n{stdout}");
-                }
-
-                return proc;
-            }
-            catch (Exception e)
-            {
-                ReportExecFailure(cmd, args, e.Message);
-                throw;
-            }
         }
 
         private static void ReportExecFailure(string cmd, string args, string msg)
@@ -207,7 +151,6 @@ namespace RabbitMQ.Client.Unit
         //
         // Flow Control
         //
-
         public static void Block(IConnection conn, Encoding encoding)
         {
             ExecRabbitMQCtl("set_vm_memory_high_watermark 0.000000001");
@@ -222,88 +165,51 @@ namespace RabbitMQ.Client.Unit
             ch.BasicPublish("amq.fanout", "", null, encoding.GetBytes("message"));
         }
 
-
         public static void Unblock()
         {
             ExecRabbitMQCtl("set_vm_memory_high_watermark 0.4");
         }
 
-        private static readonly Regex s_getConnectionProperties = new Regex(@"(?<pid>.*)\s\[.*\""connection_name\"",\""(?<connection_name>.*?)\"".*\]", RegexOptions.Compiled);
-        public class ConnectionInfo
-        {
-            public string Pid
-            {
-                get; set;
-            }
-
-            public string Name
-            {
-                get; set;
-            }
-
-            public ConnectionInfo(string pid, string name)
-            {
-                Pid = pid;
-                Name = name;
-            }
-
-            public override string ToString()
-            {
-                return $"pid = {Pid}, name: {Name}";
-            }
-        }
-
-        public static List<ConnectionInfo> ListConnections()
-        {
-            Process proc = ExecRabbitMQCtl("list_connections --silent pid client_properties");
-            string stdout = proc.StandardOutput.ReadToEnd();
-
-            try
-            {
-                // {Environment.NewLine} is not sufficient
-                var matches = s_getConnectionProperties.Matches(stdout);
-                if (matches.Count > 0)
-                {
-                    var list = new List<ConnectionInfo>(matches.Count);
-                    for (int i = 0; i < matches.Count; i++)
-                    {
-                        var s = matches[i];
-                        Debug.Assert(s.Success, "Unable to parse connection list.");
-                        Debug.Assert(s.Groups.ContainsKey("pid"), "Unable to parse pid from {stdout}");
-                        Debug.Assert(s.Groups.ContainsKey("connection_name"), "Unable to parse connection_name from {stdout}");
-                        list.Add(new ConnectionInfo(s.Groups["pid"].Value, s.Groups["connection_name"].Value));
-                    }
-
-                    return list;
-                }
-
-                return null;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine($"Bad response from rabbitmqctl list_connections --silent pid client_properties{Environment.NewLine}{stdout}");
-                throw;
-            }
-        }
-
         public static void CloseConnection(IConnection conn)
         {
-            ConnectionInfo ci = ListConnections().First(x => conn.ClientProvidedName == x.Name);
-            CloseConnection(ci.Pid);
+            CloseConnection(GetConnectionPid(conn.ClientProvidedName));
+        }
+
+        private static readonly Regex s_getConnectionProperties = new Regex(@"^(?<pid><[^>]*>)\s\[.*""connection_name"",""(?<connection_name>[^""]*)"".*\]$", RegexOptions.Multiline | RegexOptions.Compiled);
+        private static string GetConnectionPid(string connectionName)
+        {
+            string stdout = ExecRabbitMQCtl("list_connections --silent pid client_properties");
+
+            var match = s_getConnectionProperties.Match(stdout);
+            while (match.Success)
+            {
+                if (match.Groups["connection_name"].Value == connectionName)
+                {
+                    return match.Groups["pid"].Value;
+                }
+
+                match = match.NextMatch();
+            }
+
+            throw new Exception($"No connection found with name: {connectionName}");
+        }
+
+        private static void CloseConnection(string pid)
+        {
+            ExecRabbitMQCtl($"close_connection \"{pid}\" \"Closed via rabbitmqctl\"");
         }
 
         public static void CloseAllConnections()
         {
-            List<ConnectionInfo> cs = ListConnections();
-            foreach (ConnectionInfo c in cs)
+            foreach (var pid in EnumerateConnectionsPid())
             {
-                CloseConnection(c.Pid);
+                CloseConnection(pid);
             }
         }
 
-        public static void CloseConnection(string pid)
+        private static string[] EnumerateConnectionsPid()
         {
-            ExecRabbitMQCtl($"close_connection \"{pid}\" \"Closed via rabbitmqctl\"");
+            return ExecRabbitMQCtl("list_connections --silent pid").Split('\n', StringSplitOptions.RemoveEmptyEntries);
         }
 
         public static void RestartRabbitMQ()
