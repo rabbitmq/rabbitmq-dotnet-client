15a87c07470810d254eb03fae786ff8a34be1e37
Adam Stachowicz <saibamenppl@gmail.com>
2024-06-28 00:45:32 +0200

    Back-port #1616 to 6.x
    Fixes #1617
    
    Update RecordedConsumer.cs
    Update RecordedExchange.cs
    Update RecordedQueue.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedBinding.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedConsumer.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedExchange.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedQueue.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedConsumer.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedBinding.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedExchange.cs
    Update projects/RabbitMQ.Client/client/impl/RecordedQueue.cs
    Update `TestRecoveringConsumerHandlerOnConnection_EventArgumentsArePassedDown`
    Update projects/Unit/TestConnectionRecovery.cs
    
    Port test from `main`

--- projects/RabbitMQ.Client/client/impl/RecordedExchange.cs
+++ projects/RabbitMQ.Client/client/impl/RecordedExchange.cs
@@ -57,7 +57,15 @@ namespace RabbitMQ.Client.Impl
 
         public RecordedExchange WithArguments(IDictionary<string, object> value)
         {
-            Arguments = value;
+            if (value is null)
+            {
+                Arguments = null;
+            }
+            else
+            {
+                Arguments = new Dictionary<string, object>(value);
+            }
+
             return this;
         }
 
