17d2903769b6ad54a0eebb61b8369a7c7af70bef
Daniel Marbach <daniel.marbach@openplace.net>
2024-09-18 19:51:38 +0000

    Restrict AsyncEventHandler arguments

--- projects/RabbitMQ.Client/client/events/AsyncEventHandler.cs
+++ projects/RabbitMQ.Client/client/events/AsyncEventHandler.cs
@@ -33,5 +33,5 @@ using System.Threading.Tasks;
 
 namespace RabbitMQ.Client.Events
 {
-    public delegate Task AsyncEventHandler<in TEvent>(object sender, TEvent @event);
+    public delegate Task AsyncEventHandler<in TEvent>(object sender, TEvent @event) where TEvent : AsyncEventArgs;
 }
