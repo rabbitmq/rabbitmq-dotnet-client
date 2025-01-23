9c6fda80088900ef7e797d9d6ffd058b7794455e
Luke Bakken <luke@bakken.io>
2025-01-22 16:50:02 -0800

    Improve error when disabling heartbeat
    Fixes #1756
    
    * Add test that demonstrates the issue.

--- projects/Test/Integration/GH/TestGitHubIssues.cs
+++ projects/Test/Integration/GH/TestGitHubIssues.cs
@@ -117,5 +117,17 @@ namespace Test.Integration.GH
 
             Assert.False(sawConnectionShutdown);
         }
+
+        [Fact]
+        public async Task TestHeartbeatTimeoutValue_GH1756()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                AutomaticRecoveryEnabled = true,
+                RequestedHeartbeat = TimeSpan.Zero,
+            };
+
+            _conn = await connectionFactory.CreateConnectionAsync("some-name");
+        }
     }
 }
